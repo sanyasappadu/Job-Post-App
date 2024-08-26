11c45be52a0a36b7a03ee00540243e8f7b76f21b
sanyasappadu <sanyasappadu4@gmail.com>
2024-08-26 10:32:05 +0530

    job card

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,15 +1,59 @@
-import { useEffect } from "react";
+import { useEffect , useState} from "react";
 import { getJobs } from "@/api/apiJobs";
 import useFetch from '@/hooks/use-fetch';
+import JobCard from "@/components/job-card";
 
-function JobListing() {
-  const {fn:fnJobs, data:dataJobs, loading:loadingJobs} = useFetch(getJobs)
-  console.log(dataJobs)
-    useEffect(()=>{ fnJobs()},[])
+import { BarLoader } from "react-spinners";
+import { useUser } from "@clerk/clerk-react";
 
+export default function JobListing() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
+
+  const { isLoaded } = useUser();
+  const {
+    loading: loadingJobs,
+    data: jobs,
+    fn: fnJobs,
+  } = useFetch(getJobs, {
+    location,
+    company_id,
+    searchQuery,
+  });
+    useEffect(()=>{ 
+      if(isLoaded) fnJobs()
+  }, [isLoaded,location, company_id,searchQuery]);
+
+    if (!isLoaded) {
+      return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+    }
+  
   return (
-    <div>JobListing</div>
+    <div>
+       <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+        Latest Jobs
+      </h1>
+      {loadingJobs && (
+        <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
+      )}
+          {loadingJobs === false && (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {jobs?.length ? (
+            jobs.map((job) => {
+              return (
+                <JobCard
+                  key={job.id}
+                  job={job}
+                  savedInit={job?.saved?.length > 0}
+                />
+              );
+            })
+          ) : (
+            <div>No Jobs Found 😢</div>
+          )}
+        </div>
+      )}
+    </div>
   )
 }
-
-export default JobListing
